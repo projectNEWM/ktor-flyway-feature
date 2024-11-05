2a9689039a28adff90fed9b1c41b05aa48eb5fec
Andrew Westberg <andrewwestberg@gmail.com>
2024-11-05 18:42:01 +0000

    Upgrade to ktor 3

--- src/main/kotlin/com/viartemev/ktor/flyway/FlywayPlugin.kt
+++ src/main/kotlin/com/viartemev/ktor/flyway/FlywayPlugin.kt
@@ -1,8 +1,9 @@
 package com.viartemev.ktor.flyway
 
-import io.ktor.server.application.*
-import io.ktor.server.config.*
-import io.ktor.util.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.Plugin
+import io.ktor.server.config.ApplicationConfigurationException
+import io.ktor.util.AttributeKey
 import org.flywaydb.core.Flyway
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -10,7 +11,9 @@ import javax.sql.DataSource
 
 private val flyWayFeatureLogger: Logger = LoggerFactory.getLogger("com.viartemev.ktor.flyway.FlywayFeature")
 
-class FlywayPlugin(configuration: Configuration) {
+class FlywayPlugin(
+    configuration: Configuration,
+) {
     private val dataSource = configuration.dataSource
     private val locations = configuration.locations
     private val schemas = configuration.schemas
@@ -21,6 +24,7 @@ class FlywayPlugin(configuration: Configuration) {
         var locations: Array<String>? = null
         var schemas: Array<String>? = null
         internal var commands: Set<FlywayCommand> = setOf(Info, Migrate)
+
         fun commands(vararg commandsToExecute: FlywayCommand) {
             commands = commandsToExecute.toSet()
         }
@@ -29,19 +33,23 @@ class FlywayPlugin(configuration: Configuration) {
     companion object Static : Plugin<Application, Configuration, FlywayPlugin> {
         override val key = AttributeKey<FlywayPlugin>("FlywayPlugin")
 
-        override fun install(pipeline: Application, configure: Configuration.() -> Unit): FlywayPlugin {
+        override fun install(
+            pipeline: Application,
+            configure: Configuration.() -> Unit,
+        ): FlywayPlugin {
             val configuration = Configuration().apply(configure)
             val flywayFeature = FlywayPlugin(configuration)
             if (configuration.dataSource == null) throw ApplicationConfigurationException("DataSource is not configured")
 
             flyWayFeatureLogger.info("Flyway migration has started")
 
-            val flyway = Flyway
-                .configure(pipeline.environment.classLoader)
-                .dataSource(flywayFeature.dataSource)
-                .also { config -> flywayFeature.locations?.let { config.locations(*it) } }
-                .also { config -> flywayFeature.schemas?.let { config.schemas(*it) } }
-                .load()
+            val flyway =
+                Flyway
+                    .configure(pipeline.environment.classLoader)
+                    .dataSource(flywayFeature.dataSource)
+                    .also { config -> flywayFeature.locations?.let { config.locations(*it) } }
+                    .also { config -> flywayFeature.schemas?.let { config.schemas(*it) } }
+                    .load()
 
             flywayFeature.commands.map { command ->
                 flyWayFeatureLogger.info("Running command: ${command.javaClass.simpleName}")
@@ -52,5 +60,4 @@ class FlywayPlugin(configuration: Configuration) {
             return flywayFeature
         }
     }
-
 }
